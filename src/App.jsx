26c777e1deb5b983abd3d8dd72146e13a64690ba
Saurav Sahu <sauravsahu.12@live.com>
2018-02-14 10:14:45 +0530

    create basic boilerplate with eslint
    create basic boilerplate with eslint

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () =>
+  (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Notes App</h1>
+      </header>
+    </div>
+  );
+
+export default App;
