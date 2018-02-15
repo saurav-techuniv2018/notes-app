f2a030b3aa2c9767662329e6fd148d064799e10c
Saurav Sahu <sauravsahu.12@live.com>
2018-02-15 14:14:43 +0530

    Updates

--- src/render.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/app/app';
-
-const render = () => {
-  ReactDOM.render(<App />, document.getElementById('root'));
-};
-
-export default render;
